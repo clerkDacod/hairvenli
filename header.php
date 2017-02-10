016ab129bce74a789ba984c6fae885b64247fd0a
Dacod <clerk.dacod@gmail.com>
2017-02-10 13:39:35 +0200

    revision two

---
+++ header.php
@@ -0,0 +1,30 @@
+<nav class="navbar navbar-default">
+	<div class="container">
+		<div class="navbar-header">
+			<a class="navbar-brand" href="index.php">Hairvenli</a>
+		</div>
+
+		<ul class="nav navbar-nav">
+	 		<li> <a href="#">Nearby</a></li>
+	 		<li> <form class="navbar-form" role="search"> 
+				 <div class="form-group has-feedback has-feedback-left">
+				  
+				 	<input type="text" class="form-control" placeholder="Search" />
+				 	<i class="glyphicon glyphicon-search form-control-feedback"></i>
+				 </div>
+
+	 		</form></li>
+	 		<li> <a href="#">Notifications</a></li>
+	 		<li > <a  href="#">Profile</a></li>
+	 	</ul>
+		
+		<div class="btn-group">
+			<button type="button" class="btn btn-default navbar-btn">Sign in</button>
+			<button type="button" class="btn btn-primary navbar-btn">Sign Up </button>
+		</div>
+	 	
+
+	</div>
+	
+</nav>
+ 
---
+++ index.php
@@ -0,0 +1,10 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+ 	<title>hairvenli - your hair beauty club</title>
+	<?php require_once("head.php"); ?>
+</head>
+	<?php require_once("header.php"); ?>
+</html>
