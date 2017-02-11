3c8983a9baae80e2e3ccc2cf3a33427e527183b0
Student <Student>
2017-02-11 17:16:34 +0200

    Stylists.sql added

---
+++ Stylists.sql
@@ -0,0 +1,64 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.5.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 11, 2017 at 04:14 PM
+-- Server version: 10.1.21-MariaDB
+-- PHP Version: 5.6.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `hairvenli`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Stylists`
+--
+
+CREATE TABLE `stylists` (
+  `id` int(11) NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `latitude` varchar(20) NOT NULL,
+  `longitude` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `Stylists`
+--
+
+INSERT INTO `Stylists` (`id`, `name`, `latitude`, `longitude`) VALUES
+(1, 'Dacod', '-33.9657883', '18.48102');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `Stylists`
+--
+ALTER TABLE `Stylists`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `Stylists`
+--
+ALTER TABLE `Stylists`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
